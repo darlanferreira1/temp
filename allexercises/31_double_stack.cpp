c768291f81306237f145c1bde3f64b83ebb5907f
darlanferreira1 <darlanferreiradof7@gmail.com>
2022-06-13 06:37:50 -0300

    up

---
+++ allexercises/31_double_stack.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+typedef struct _{
+    std::stack <int> storer;
+    std::stack <int> harvest;
+} My_queue;
+
+int main (){
+    int n {}, m {};
+    std::cin >> n >> m;
+    
+}
